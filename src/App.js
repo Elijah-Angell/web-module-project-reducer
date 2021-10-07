46619a9d3760d5337ed3270f7e3d408cf5e45f03
user <useruser@Guests-MacBook-Pro.local>
2021-10-07 15:51:21 -0400

    state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import reducer, { initialState } from './reducers';
-import './App.css';
+import React, { useReducer } from 'react';
 
+import './App.css';
+import reducer, { initialState } from './reducers';
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 function App() {
-  // const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   // const handleAddButton = (value) => {
   //   dispatch(addValue(value));
@@ -30,10 +30,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
 
-            <TotalDisplay value={0} />
+            <TotalDisplay value={state.total} />
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
 
             <div className="row">
